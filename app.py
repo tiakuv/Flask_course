b9c91e568b53cec92f6a0740d5b62884aa8bcf20
tiakuv <tiakuv@gmail.com>
2020-05-01 00:38:17 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    output = render_template("index.html")
+    return output
+
+@app.route('/departures/<departure>')
+def show_dep(departure):
+    output = render_template("departure.html")
+    return output
+
+@app.route('/tours/<id>')
+def show_tour(id):
+    output = render_template("tour.html")
+    return output
+
+app.run('localhost', 8000)
